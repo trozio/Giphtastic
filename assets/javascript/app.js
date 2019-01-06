209d5655b20b426156c9ccdd688c8678d5c16c48
trozio <daniel.l.adelman@gmail.com>
2019-01-05 21:41:40 -0500

    css and logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,48 @@
-let topics = ["animals", "cars", "houses", "sports"];
+let topics = ["ANIMALS", "CARS", "CATS", "SPORTS"];
 	displayButtons();
 $("#add-gif").on("click", function(event){
 	event.preventDefault();
-	let userInput = $("#searchBar").val().trim();
+	let userInput = $("#searchBar").val().trim().toUpperCase();
 	topics.push(userInput);
 	console.log(topics);
 	$("#searchBar").val("");
 	$(".buttonsDisplay").empty();
 	displayButtons();
-
 })
 function displayButtons(){
 	for(i = 0; i < topics.length; i++){
-
 		let gifButton = $("<button>");
-          gifButton.addClass("gif");
-          gifButton.attr("data-name", topics[i]);
-          gifButton.text(topics[i]);
-          $(".buttonsDisplay").append(gifButton);
-        }
-      }
-	function displayGif(){
+        gifButton.addClass("gif");
+        gifButton.attr("data-name", topics[i]);
+        gifButton.text(topics[i]);
+        $(".buttonsDisplay").append(gifButton);
 
-  		let gif = $(".gif").attr("data-name");
-  		let key = "klPo6zVIqSFJBs82gW1NEQAWIL7WaKax";
-  		let queryURL = "https://api.giphy.com/v1/gifs/search?" + "api_key=" + key + "&q=" + gif + "&limit=25&offset=0&rating=G&lang=en"
+    }
+	$(".gif").on("click", function(){
+		$(".gifs").empty();
+		let gif = $(this).attr("data-name");
+		let state = $(this).attr("data-state");
+		let key = "klPo6zVIqSFJBs82gW1NEQAWIL7WaKax";
+		let queryURL = "https://api.giphy.com/v1/gifs/search?" + "api_key=" + key + "&q=" + gif + "&limit=25&offset=0&rating=G&lang=en"
 
-  		$.ajax({
-  			 url: queryURL,
-  			 method: "GET"
-  		   }).then(function(response) {
-  			   console.log(response);
-			   for (var j = 0; j < response.data.length; j++) {
+		$.ajax({
+			 url: queryURL,
+			 method: "GET"
+		   }).then(function(response) {
+			   console.log(response);
+			   for (var j = 0; j < 10; j++) {
 			   let gifImage = $("<img>").attr("src", response.data[j].images.fixed_height.url);
 			   let gifView = $("<div>");
+			   let rating = "Rating: " + response.data[j].rating.toUpperCase();
 			   gifView.append(gifImage);
+			   gifView.append(rating);
 			   $(".gifs").prepend(gifView);
-		   }
-  	})
-  	}
-$(".gif").on("click", function(){
-	$(".gifs").empty();
-	displayGif();
+
+											}
+										})
+									})
+
+								}
+$("reset").on("click", function(){
+	$(".buttonsDisplay").empty();
 })
